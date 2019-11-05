833f13b50d84dc7e8c6abc8263a19f8a4a10c742
ipek <ipekcivicakan@gmail.com>
2019-11-05 14:46:38 -0500

    plugins added to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './app/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index_bundle.js'
-    }
-},
-    {
-        module: {
-            rules: [
-                { test: /\.(js)$/, use: 'babel-loader' }, //* Any js file inside of the application, go ahead and run the babel-loader on it.
-                { test: /\.css$/, use: ['style-loader', 'css-loader'] }
-            ]
-        },
-        mode: 'development'
-    }
+    },
+    module: {
+        rules: [
+            { test: /\.(js)$/, use: 'babel-loader' }, //* Any js file inside of the application, go ahead and run the babel-loader on it.
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+        ]
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'app/index.html'
+        })
+    ]
+}
